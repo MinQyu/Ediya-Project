f5a6c9d69c51616359f22145e7aff889f013114b
MinQyu <jorushs@gmail.com>
2024-02-23 19:24:22 +0900

    레이아웃 방식 구현, css 수정

--- src/route/Home.tsx
+++ src/route/Home.tsx
@@ -1,13 +1,5 @@
-import Header from "../components/Header";
-
 function Home() {
-  return (
-    <div>
-      <div>
-        <Header />
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default Home;
