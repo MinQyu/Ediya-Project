d91dc107d6516f0ec526ca6cadae7abcee3c8b6a
MinQyu <jorushs@gmail.com>
2024-03-08 20:24:59 +0900

    검색기능 추가

--- src/components/NewsList.tsx
+++ src/components/NewsList.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import {
   Articles,
   ArticleData,
@@ -8,13 +8,14 @@ import {
 import styles from "./NewsList.module.css";
 
 function NewsList() {
-  const [loading, setLoading] = useState<boolean>(true);
   const [articleList, setArticleList] = useState<ArticleData>();
   const [request, setRequest] = useState<ArticleRequest>({
     keyword: "",
     keywordType: "",
     currentPage: 1
   });
+  const selectRef = useRef<HTMLSelectElement>(null);
+  const searchRef = useRef<HTMLInputElement>(null);
   const parseArticleJson = (json: ArticleData): ArticleData => {
     const articles: Articles[] = json.articles.map((article: Articles) => ({
       sn: article.sn,
@@ -46,7 +47,7 @@ function NewsList() {
     ).json();
     const parsedJson = parseArticleJson(json);
     setArticleList(parsedJson);
-    setLoading(false);
+    console.log(parsedJson);
   };
 
   const formatDate = (date: Date): string => {
@@ -60,6 +61,13 @@ function NewsList() {
     return text.length > 61 ? text.slice(0, 61) + " ..." : text;
   };
 
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const keywordType = selectRef.current?.value ?? "title";
+    const keyword = searchRef.current?.value ?? "";
+    setRequest({ keywordType, keyword, currentPage: 1 });
+  };
+
   useEffect(() => {
     getArticleList();
   }, [request]);
@@ -67,18 +75,19 @@ function NewsList() {
   return (
     <div className={styles.container}>
       <div className={styles.filter}>
-        <select>
+        <select ref={selectRef}>
           <option value={"title"}>제목</option>
           <option value={"content"}>내용</option>
         </select>
-        <div className={styles.search_bar}>
-          <input></input>
+        <form className={styles.search_bar} onSubmit={handleSubmit}>
+          <input ref={searchRef}></input>
           <button
             style={{
               backgroundImage: `url(${process.env.PUBLIC_URL}/assets/검색.svg)`
             }}
+            type="submit"
           />
-        </div>
+        </form>
       </div>
       <table>
         <thead>
@@ -88,88 +97,29 @@ function NewsList() {
             <th>날짜</th>
           </tr>
         </thead>
-        <tbody>
-          <tr>
-            <td className={styles.table__sn}>
-              {articleList && articleList.articles[0].sn}
-            </td>
-            <td>
-              <img src={articleList && articleList.articles[0].imgSrc} />
-              <h3>{articleList && articleList.articles[0].title}</h3>
-              <p>
-                {articleList && formatText(articleList.articles[0].content)}
-              </p>
-            </td>
-            <td className={styles.table__date}>
-              {articleList &&
-                formatDate(articleList.articles[0].registrationDate)}
-            </td>
-          </tr>
-          <tr>
-            <td className={styles.table__sn}>
-              {articleList && articleList.articles[1].sn}
-            </td>
-            <td>
-              <img src={articleList && articleList.articles[1].imgSrc} />
-              <h3>{articleList && articleList.articles[1].title}</h3>
-              <p>
-                {articleList && formatText(articleList.articles[1].content)}
-              </p>
-            </td>
-            <td className={styles.table__date}>
-              {articleList &&
-                formatDate(articleList.articles[1].registrationDate)}
-            </td>
-          </tr>
-          <tr>
-            <td className={styles.table__sn}>
-              {articleList && articleList.articles[2].sn}
-            </td>
-            <td>
-              <img src={articleList && articleList.articles[2].imgSrc} />
-              <h3>{articleList && articleList.articles[2].title}</h3>
-              <p>
-                {articleList && formatText(articleList.articles[2].content)}
-              </p>
-            </td>
-            <td className={styles.table__date}>
-              {articleList &&
-                formatDate(articleList.articles[2].registrationDate)}
-            </td>
-          </tr>
-          <tr>
-            <td className={styles.table__sn}>
-              {articleList && articleList.articles[3].sn}
-            </td>
-            <td>
-              <img src={articleList && articleList.articles[3].imgSrc} />
-              <h3>{articleList && articleList.articles[3].title}</h3>
-              <p>
-                {articleList && formatText(articleList.articles[3].content)}
-              </p>
-            </td>
-            <td className={styles.table__date}>
-              {articleList &&
-                formatDate(articleList.articles[3].registrationDate)}
-            </td>
-          </tr>
-          <tr>
-            <td className={styles.table__sn}>
-              {articleList && articleList.articles[4].sn}
-            </td>
-            <td>
-              <img src={articleList && articleList.articles[4].imgSrc} />
-              <h3>{articleList && articleList.articles[4].title}</h3>
-              <p>
-                {articleList && formatText(articleList.articles[4].content)}
-              </p>
-            </td>
-            <td className={styles.table__date}>
-              {articleList &&
-                formatDate(articleList.articles[4].registrationDate)}
-            </td>
-          </tr>
-        </tbody>
+        {articleList ? (
+          articleList.articles.length === 0 ? (
+            <div className={styles.no_list}>검색 결과가 없습니다</div>
+          ) : (
+            <tbody>
+              {articleList.articles.map((article, index) => (
+                <tr key={index}>
+                  <td className={styles.table__sn}>{article.sn}</td>
+                  <td>
+                    <img src={article.imgSrc} />
+                    <h3>{article.title}</h3>
+                    <p>{formatText(article.content)}</p>
+                  </td>
+                  <td className={styles.table__date}>
+                    {formatDate(article.registrationDate)}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          )
+        ) : (
+          <div></div>
+        )}
       </table>
       <div className={styles.pagination}></div>
     </div>
